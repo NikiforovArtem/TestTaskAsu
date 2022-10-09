a338bb35cb98cb7af06a2c4d67b65f10cb605550
Artem <nikart28@gmail.com>
2022-10-09 22:42:09 +0600

    Add table header

--- AsuTaskFe/src/components/Dashboard.tsx
+++ AsuTaskFe/src/components/Dashboard.tsx
@@ -1,6 +1,8 @@
 import BasicLayout from 'common/components/BasicLayout';
-import React, {memo, useCallback, useEffect, useState} from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import {
+  Box,
+  Button,
   MenuItem,
   Pagination,
   Select,
@@ -14,88 +16,96 @@ import {
   TableRow
 } from '@mui/material';
 import BookService from 'services/BookService';
-import {useCommunication} from 'common/hooks/useCommunication';
-import {IRequestParams} from 'common/models/IRequestParams';
-import {IBook} from 'services/models/IBook';
+import { useCommunication } from 'common/hooks/useCommunication';
+import { IRequestParams } from 'common/models/IRequestParams';
+import { IBook } from 'services/models/IBook';
 import ErrorMessage from 'common/components/ErrorMessage';
-import {IListResponse} from 'common/models/IResponse';
+import { IListResponse } from 'common/models/IResponse';
+import AuthService from 'services/AuthService';
 
-const ROWS_PER_PAGE_ITEMS = [5, 10, 15, 20];
+const ROWS_PER_PAGE_ITEMS = [2, 4, 10, 20];
 
 const Dashboard = () => {
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowPerPage] = useState(2);
 
-  const {data, request, communication} = useCommunication<IRequestParams, IListResponse<IBook>>(BookService.getBooks)
+  const { data, request, communication } = useCommunication<IRequestParams, IListResponse<IBook>>(BookService.getBooks)
   const isLoading = communication.type === 'requesting';
   const errorMessage = communication.error;
 
   const onChangePage = useCallback((event: React.ChangeEvent<unknown>, value: number) => setPage(value), [setPage]);
   const onChangeRowsPerPage = useCallback((event: SelectChangeEvent<number>) => setRowPerPage(+event.target.value), [setRowPerPage]);
 
-  useEffect(() => request({pageSize: rowsPerPage, pageNumber: page}), [request, rowsPerPage, page]);
+  useEffect(() => request({ pageSize: rowsPerPage, pageNumber: page }), [request, rowsPerPage, page]);
 
   return (
-    <BasicLayout isLoading={isLoading}>
-      <Stack spacing={4}>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <b>ID</b>
-                </TableCell>
-                <TableCell align="right">
-                  <b>Name</b>
-                </TableCell>
-                <TableCell align="right">
-                  <b>Genre</b>
-                </TableCell>
-                <TableCell align="right">
-                  <b>Author</b>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data?.data?.map((row) => (
-                <TableRow key={row.id}>
-                  <TableCell>{row.id}</TableCell>
-                  <TableCell align="right">{row.name}</TableCell>
-                  <TableCell align="right">{row.genre}</TableCell>
-                  <TableCell align="right">{row.authorName}</TableCell>
+    <>
+      <Box position="absolute" sx={{ top: '20px', right: '20px' }}>
+        <Button onClick={AuthService.logout} variant="contained">LOGOUT</Button>
+      </Box>
+
+      <BasicLayout isLoading={isLoading}>
+        <Stack spacing={5}>
+        <BasicLayout><h2>Books table</h2></BasicLayout>
+          <TableContainer>
+            <Table aria-label="Books">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <b>ID</b>
+                  </TableCell>
+                  <TableCell align="right">
+                    <b>Name</b>
+                  </TableCell>
+                  <TableCell align="right">
+                    <b>Genre</b>
+                  </TableCell>
+                  <TableCell align="right">
+                    <b>Author</b>
+                  </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {data?.data?.map((row) => (
+                  <TableRow key={row.id}>
+                    <TableCell>{row.id}</TableCell>
+                    <TableCell align="right">{row.name}</TableCell>
+                    <TableCell align="right">{row.genre}</TableCell>
+                    <TableCell align="right">{row.authorName}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
 
-        {data?.data && (
-          <Stack direction="row" alignItems="center">
-            <div style={{width: 180}}>Rows per page:</div>
-            <Select
-              style={{width: 70, height: 40}}
-              value={rowsPerPage}
-              label=""
-              autoWidth={true}
-              onChange={onChangeRowsPerPage}
-            >
-              {ROWS_PER_PAGE_ITEMS.map(item => (
-                <MenuItem key={item} value={item}>{item}</MenuItem>
-              ))}
-            </Select>
+          {data && (
+            <Stack direction="row" alignItems="center">
+              <div style={{ width: 180 }}>Rows per page:</div>
+              <Select
+                style={{ width: 70, height: 40 }}
+                value={rowsPerPage}
+                label=""
+                autoWidth={true}
+                onChange={onChangeRowsPerPage}
+              >
+                {ROWS_PER_PAGE_ITEMS.map(item => (
+                  <MenuItem key={item} value={item}>{item}</MenuItem>
+                ))}
+              </Select>
 
-            <Pagination
-              sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%'}}
-              count={data.totalPages}
-              page={data.currentPage}
-              onChange={onChangePage}
-            />
-          </Stack>
-        )}
-        <ErrorMessage message={errorMessage}/>
-      </Stack>
+              <Pagination
+                sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}
+                count={data.totalPages}
+                page={data.currentPage}
+                onChange={onChangePage}
+              />
+            </Stack>
+          )}
+          <ErrorMessage message={errorMessage} />
+        </Stack>
 
-    </BasicLayout>
+      </BasicLayout>
+    </>
   )
 }
 
